94d04248eb28e50ce8e20e844d94c45670c46a09
Aaron Bauer <abauer@janestreet.com>
2026-06-12 12:15:51 -0400

    testing CI test and format failures

--- lib/types/src/book.ml
+++ lib/types/src/book.ml
@@ -2,8 +2,7 @@ open! Core
 
 type t =
   { symbol : Symbol.t
-  ; bids : Leve.t list
-  ; asks : Level.t list
+  ; bids : Level.t list ; asks : Level.t list
   ; bbo : Bbo.t
   }
 [@@deriving sexp, bin_io]
